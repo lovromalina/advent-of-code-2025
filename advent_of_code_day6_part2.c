c8a71721d42e853d32e5812e63e7a5ec77606ec7
Lovro Malinarić <lovromalinaric5@gmail.com>
2025-12-06 15:02:54 +0100

    shorter day6 part2

--- advent_of_code_day6_part2.c
+++ advent_of_code_day6_part2.c
@@ -15,6 +15,8 @@ int main(){
 	unsigned long long sum = 0;
 	unsigned long long mul = 1;
 	int exp = 0;
+	int num = 0;
+
 	for (int i = 0; i < ROWS; ++i)
 	{
 		fgets(grid[i], SIZE, stdin);
@@ -22,83 +24,46 @@ int main(){
 
 	op = grid[ROWS-1];
 
-	//printf("%s\n", op);
-	/*
-	for (int i = 0; i < ROWS; ++i)
-	{
-		printf("%s\n", grid[i]);
-	}
-	
-	for (int i = 0; i < strlen(op); ++i)
-	{
-		printf("%c\n", op[i]);
-	}
-	*/
-
-	for (int i = 0; i < strlen(op); ++i)
-	{
+	for (int i = 0; i < strlen(op); ++i){
 		if(op[i] == ' ') {
 			continue;
 		}
 
-		if(op[i] == '+'){
-			is_space = 1;	
+		is_space = 1;	
 
-			for(j = i; ; j++){
-				for (int z = ROWS-2; z >= 0 ; --z){
-					if(grid[z][j] == '\n'){
-						break;
-					}
-					if(grid[z][j] != ' '){
-						is_space = 0;
-						sum += (grid[z][j] -'0')*pow(10, exp++);
+		for(j = i; ; j++){
+			for (int z = ROWS-2; z >= 0 ; --z){
 
-
-					}
-					
-				}
-				if(is_space){
-					//printf("break\n");
+				if(grid[z][j] == '\n'){
 					break;
 				}
-				is_space = 1;
-				//printf(" %d\n", sum);
-				res += sum;
-				sum = 0;
-				exp = 0;
-				
-			}
-		}else{
-			is_space = 1;	
 
-			for(j = i; ; j++){
-				for (int z = ROWS-2; z >= 0 ; --z){
-					if(grid[z][j] == '\n'){
-						break;
-					}
-					if(grid[z][j] != ' '){
-						is_space = 0;
-						sum += (grid[z][j] -'0')*pow(10, exp++);
-
-					}
-					
-				}
-				exp = 0;
-				if(is_space){
-					//printf("break\n");
-					break;
-				}
-				is_space = 1;
-				//printf("%d\n", sum);
-				mul *= sum;
-				//printf("mul = %d\n", mul);
-				sum = 0;
-				
+				if(grid[z][j] != ' '){
+					is_space = 0;
+					num += (grid[z][j] -'0')*pow(10, exp++);
+				}	
 			}
-			res += mul;
-			mul = 1;
-			sum = 0;
+
+			if(is_space){
+				break;
+			}
+
+			if(op[i] == '+'){
+				sum += num;
+				
+			}else{
+				mul *= num;
+			}
+			
+			is_space = 1;
+			exp = 0;
+			num = 0;
 		}
+
+		if(mul != 1)res += mul;
+		else res += sum;
+		mul = 1;
+		sum = 0;
 		
 	}
 	printf("%llu\n", res);
